9151dbab180afdcc179d84a8dfa2c7ee770c4722
Rubén Caro <ruben.caro.estevez@gmail.com>
2017-04-05 20:30:49 +0200

    First commit

---
+++ lib/cardo.ex
@@ -0,0 +1,18 @@
+defmodule Cardo do
+  @moduledoc """
+  Documentation for Cardo.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Cardo.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/cardo_test.exs
@@ -0,0 +1,8 @@
+defmodule CardoTest do
+  use ExUnit.Case
+  doctest Cardo
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
